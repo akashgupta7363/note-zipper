229d9829f8abf7ef573bb8a40f12e4c0c9d52bef
Akash Gupta <akashgupta7363@gmail.com>
2023-05-13 17:04:26 +0530

    producton mode

--- backend/server.js
+++ backend/server.js
@@ -20,19 +20,19 @@ app.use("/api/notes", noteRoutes);
 app.use("/api/users", userRoutes);
 
 // // --------------------------deployment------------------------------
-// const __dirname = path.resolve();
+const __dirname = path.resolve();
 
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static(path.join(__dirname, "/frontend/build")));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
 
-//   app.get("*", (req, res) =>
-//     res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
-//   );
-// } else {
-//   app.get("/", (req, res) => {
-//     res.send("API is running..");
-//   });
-// }
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running..");
+  });
+}
 // --------------------------deployment------------------------------
 
 // Error Handling middlewares
