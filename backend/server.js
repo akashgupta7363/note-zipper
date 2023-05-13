49911f0aa5dd9bf78709f614d2e0a68017f0aa7b
Akash Gupta <akashgupta7363@gmail.com>
2023-05-13 15:13:43 +0530

    modified : server.js

--- backend/server.js
+++ backend/server.js
@@ -12,27 +12,27 @@ dotenv.config();
 
 connectDB();
 
-const app = express(); // main thing
+const app = express();
 
-app.use(express.json()); // to accept json data
+app.use(express.json());
 
 app.use("/api/notes", noteRoutes);
 app.use("/api/users", userRoutes);
 
-// --------------------------deployment------------------------------
-const __dirname = path.resolve();
+// // --------------------------deployment------------------------------
+// const __dirname = path.resolve();
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/build")));
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "/frontend/build")));
 
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
-  );
-} else {
-  app.get("/", (req, res) => {
-    res.send("API is running..");
-  });
-}
+//   app.get("*", (req, res) =>
+//     res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+//   );
+// } else {
+//   app.get("/", (req, res) => {
+//     res.send("API is running..");
+//   });
+// }
 // --------------------------deployment------------------------------
 
 // Error Handling middlewares
