f506d4ddbe207475eed9b5fb67567528902f40d7
Akash Gupta <akashgupta7363@gmail.com>
2023-05-13 17:52:16 +0530

    Revert "fixing"
    This reverts commit b2945c2c8879e05e99e2c793beff1f2a27c8a29d.

--- backend/server.js
+++ backend/server.js
@@ -20,24 +20,20 @@ app.use("/api/notes", noteRoutes);
 app.use("/api/users", userRoutes);
 
 // // --------------------------deployment------------------------------
-// const __dirname = path.resolve();
+const __dirname = path.resolve();
 
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static(path.join(__dirname, "../frontend/build")));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
 
-//   app.get("*", (req, res) =>
-//     res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
-//   );
-// } else {
-//   app.get("/", (req, res) => {
-//     res.send("API is running..");
-//   });
-// }
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running..");
+  });
+}
 // --------------------------deployment------------------------------
-
-app.get("/", (req, res) => {
-  res.send("API is running..");
-});
 
 // Error Handling middlewares
 app.use(notFound);
